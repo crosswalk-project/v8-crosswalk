fb3825a369cbf53f0e751b82c5f0c31cce1ba201
Haitao Feng <haitao.feng@intel.com>
2014-03-05 15:33:11 +0800

    [SIMD.js] Inline all SIMD operations
    The implementation is only for ia32 and x64 ports.
    
    Optimize Float32x4Array, Float64x2Array and Int32x4Array element loading
    and storing for ia32 and x64 port by:
      1). Allocating SIMD128 registers
      2). Emitting SIMD movups instruction
      3). Adding deoptimization support for SIMD128 registers or stack slots
    
    Inline all SIMD operations and emit SIMD instructions for ia32 and x64 port
    
    Conflicts:
    	src/ia32/assembler-ia32.cc
    	src/ia32/disasm-ia32.cc
    	src/x64/assembler-x64.cc
    	src/x64/assembler-x64.h
    	src/x64/disasm-x64.cc
    	src/x64/lithium-codegen-x64.cc
    	src/x64/lithium-x64.h

--- src/hydrogen.h
+++ src/hydrogen.h
@@ -2457,14 +2457,16 @@ class HOptimizedGraphBuilder : public HGraphBuilder, public AstVisitor {
     PropertyAccessInfo(HOptimizedGraphBuilder* builder,
                        PropertyAccessType access_type,
                        Type* type,
-                       Handle<String> name)
+                       Handle<String> name,
+                       InstanceType instance_type)
         : lookup_(builder->isolate()),
           builder_(builder),
           access_type_(access_type),
           type_(type),
           name_(name),
           field_type_(HType::Tagged()),
-          access_(HObjectAccess::ForMap()) { }
+          access_(HObjectAccess::ForMap()),
+          instance_type_(instance_type) { }
 
     // Checkes whether this PropertyAccessInfo can be handled as a monomorphic
     // load named. It additionally fills in the fields necessary to generate the
@@ -2525,6 +2527,24 @@ class HOptimizedGraphBuilder : public HGraphBuilder, public AstVisitor {
 
     bool IsConfigurable() const { return lookup_.IsConfigurable(); }
     bool IsReadOnly() const { return lookup_.IsReadOnly(); }
+    bool IsSIMD128PropertyCallback() {
+      return (((instance_type_ == Float32x4::kInstanceType ||
+                instance_type_ == Int32x4::kInstanceType) &&
+               (name_->Equals(isolate()->heap()->signMask()) ||
+                name_->Equals(isolate()->heap()->x()) ||
+                name_->Equals(isolate()->heap()->y()) ||
+                name_->Equals(isolate()->heap()->z()) ||
+                name_->Equals(isolate()->heap()->w()))) ||
+              (instance_type_ == Int32x4::kInstanceType &&
+               (name_->Equals(isolate()->heap()->flagX()) ||
+                name_->Equals(isolate()->heap()->flagY()) ||
+                name_->Equals(isolate()->heap()->flagZ()) ||
+                name_->Equals(isolate()->heap()->flagW()))) ||
+              (instance_type_ == Float64x2::kInstanceType &&
+               (name_->Equals(isolate()->heap()->signMask()) ||
+                name_->Equals(isolate()->heap()->x()) ||
+                name_->Equals(isolate()->heap()->y()))));
+    }
 
    private:
     Handle<Object> GetAccessorsFromMap(Handle<Map> map) const {
@@ -2573,6 +2593,7 @@ class HOptimizedGraphBuilder : public HGraphBuilder, public AstVisitor {
     SmallMapList field_maps_;
     HType field_type_;
     HObjectAccess access_;
+    InstanceType instance_type_;
   };
 
   HInstruction* BuildMonomorphicAccess(PropertyAccessInfo* info,
