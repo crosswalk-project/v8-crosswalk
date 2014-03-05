dc5d4552dd5d832869a6f906c1d987e8904ea705
Haitao Feng <haitao.feng@intel.com>
2014-03-05 15:33:11 +0800

    [SIMD.js] Inline all SIMD operations
    The implementation is only for ia32 and x64 ports.
    
    Optimize Float32x4Array, Float64x2Array and Int32x4Array element loading
    and storing for ia32 and x64 port by:
      1). Allocating SIMD128 registers
      2). Emitting SIMD movups instruction
      3). Adding deoptimization support for SIMD128 registers or stack slots
    
    Inline all SIMD operations and emit SIMD instructions for ia32 and x64 port
    
    Conflicts:
    
    	src/arm/lithium-codegen-arm.cc
    	src/hydrogen-types.h
    	src/hydrogen.cc
    	src/hydrogen.h
    	src/ia32/assembler-ia32.h
    	src/ia32/lithium-codegen-ia32.cc
    	src/x64/lithium-codegen-x64.cc

--- src/arm64/deoptimizer-arm64.cc
+++ src/arm64/deoptimizer-arm64.cc
@@ -112,6 +112,9 @@ void Deoptimizer::CopyDoubleRegisters(FrameDescription* output_frame) {
 }
 
 
+void Deoptimizer::CopySIMD128Registers(FrameDescription* output_frame) {
+}
+
 
 #define __ masm()->
 
@@ -356,6 +359,41 @@ void FrameDescription::SetCallerFp(unsigned offset, intptr_t value) {
 void FrameDescription::SetCallerConstantPool(unsigned offset, intptr_t value) {
   // No out-of-line constant pool support.
   UNREACHABLE();
+}
+
+
+double FrameDescription::GetDoubleRegister(unsigned n) const {
+  DCHECK(n < arraysize(double_registers_));
+  return double_registers_[n];
+}
+
+
+void FrameDescription::SetDoubleRegister(unsigned n, double value) {
+  DCHECK(n < arraysize(double_registers_));
+  double_registers_[n] = value;
+}
+
+
+simd128_value_t FrameDescription::GetSIMD128Register(unsigned n) const {
+  UNREACHABLE();
+  simd128_value_t value;
+  return value;
+}
+
+
+void FrameDescription::SetSIMD128Register(unsigned n, simd128_value_t value) {
+  UNREACHABLE();
+}
+
+
+int FrameDescription::double_registers_offset() {
+  return OFFSET_OF(FrameDescription, double_registers_);
+}
+
+
+int FrameDescription::simd128_registers_offset() {
+  UNREACHABLE();
+  return -1;
 }
 
 
