c95097938ad652be39b1fa2306acbced01f6a5b3
Ningxin Hu <ningxin.hu@intel.com>
2014-05-25 14:38:37 +0800

    Make CanDeoptimize() check of kStoreKeyed depend on elements_kind.
    If the elements_kind is SIMD128 and Crankshaft doesn't support SIMD128,
    kStoreKeyed can deoptimized.

--- src/hydrogen-instructions.cc
+++ src/hydrogen-instructions.cc
@@ -872,7 +872,6 @@ bool HInstruction::CanDeoptimize() {
     case HValue::kSar:
     case HValue::kSeqStringGetChar:
     case HValue::kStoreCodeEntry:
-    case HValue::kStoreKeyed:
     case HValue::kStoreNamedField:
     case HValue::kStoreNamedGeneric:
     case HValue::kStringCharCodeAt:
@@ -882,6 +881,10 @@ bool HInstruction::CanDeoptimize() {
     case HValue::kUnknownOSRValue:
     case HValue::kUseConst:
       return false;
+
+    case HValue::kStoreKeyed:
+      return !CpuFeatures::SupportsSIMD128InCrankshaft() &&
+             IsSIMD128ElementsKind(HStoreKeyed::cast(this)->elements_kind());
 
     case HValue::kAdd:
     case HValue::kApplyArguments:
