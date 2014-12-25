a8f710a24041eb963f671a86b196b4de461db3ac
Ningxin Hu <ningxin.hu@intel.com>
2014-12-25 12:47:23 +0800

    [SIMD.js] Optimize Emscripten generated load and store API
    1. Inline SIMD type load and store operations
    2. Optimize SIMD load and store with Uint8Array
    
    BUG=XWALK-3371
    
    Conflicts:
    	src/hydrogen.h

--- src/hydrogen-instructions.h
+++ src/hydrogen-instructions.h
@@ -6884,12 +6884,18 @@ class HLoadKeyed FINAL
           op_ == kInt8ArrayGetFloat32x4XYZW ||
           op_ == kInt8ArrayGetFloat32x4X ||
           op_ == kInt8ArrayGetFloat32x4XY ||
-          op_ == kInt8ArrayGetFloat32x4XYZ) {
+          op_ == kInt8ArrayGetFloat32x4XYZ ||
+          op_ == kUint8ArrayGetFloat32x4XYZW ||
+          op_ == kUint8ArrayGetFloat32x4X ||
+          op_ == kUint8ArrayGetFloat32x4XY ||
+          op_ == kUint8ArrayGetFloat32x4XYZ) {
         set_representation(Representation::Float32x4());
       } else if (op_ == kFloat64ArrayGetFloat64x2XY ||
           op_ == kFloat64ArrayGetFloat64x2X ||
           op_ == kInt8ArrayGetFloat64x2XY ||
-          op_ == kInt8ArrayGetFloat64x2X) {
+          op_ == kInt8ArrayGetFloat64x2X ||
+          op_ == kUint8ArrayGetFloat64x2XY ||
+          op_ == kUint8ArrayGetFloat64x2X) {
         set_representation(Representation::Float64x2());
       } else if (op_ == kInt32ArrayGetInt32x4XYZW ||
           op_ == kInt32ArrayGetInt32x4X ||
@@ -6898,7 +6904,11 @@ class HLoadKeyed FINAL
           op_ == kInt8ArrayGetInt32x4XYZW ||
           op_ == kInt8ArrayGetInt32x4X ||
           op_ == kInt8ArrayGetInt32x4XY ||
-          op_ == kInt8ArrayGetInt32x4XYZ) {
+          op_ == kInt8ArrayGetInt32x4XYZ ||
+          op_ == kUint8ArrayGetInt32x4XYZW ||
+          op_ == kUint8ArrayGetInt32x4X ||
+          op_ == kUint8ArrayGetInt32x4XY ||
+          op_ == kUint8ArrayGetInt32x4XYZ) {
         set_representation(Representation::Int32x4());
       } else if (elements_kind == EXTERNAL_FLOAT32_ELEMENTS ||
           elements_kind == EXTERNAL_FLOAT64_ELEMENTS ||
@@ -7244,12 +7254,18 @@ class HStoreKeyed FINAL
         op_ == kInt8ArraySetFloat32x4XYZW ||
         op_ == kInt8ArraySetFloat32x4X ||
         op_ == kInt8ArraySetFloat32x4XY ||
-        op_ == kInt8ArraySetFloat32x4XYZ) {
+        op_ == kInt8ArraySetFloat32x4XYZ ||
+        op_ == kUint8ArraySetFloat32x4XYZW ||
+        op_ == kUint8ArraySetFloat32x4X ||
+        op_ == kUint8ArraySetFloat32x4XY ||
+        op_ == kUint8ArraySetFloat32x4XYZ) {
       return Representation::Float32x4();
     } else if (op_ == kFloat64ArraySetFloat64x2XY ||
         op_ == kFloat64ArraySetFloat64x2X ||
         op_ == kInt8ArraySetFloat64x2XY ||
-        op_ == kInt8ArraySetFloat64x2X) {
+        op_ == kInt8ArraySetFloat64x2X ||
+        op_ == kUint8ArraySetFloat64x2XY ||
+        op_ == kUint8ArraySetFloat64x2X) {
       return Representation::Float64x2();
     } else if (op_ == kInt32ArraySetInt32x4XYZW ||
         op_ == kInt32ArraySetInt32x4X ||
@@ -7258,7 +7274,11 @@ class HStoreKeyed FINAL
         op_ == kInt8ArraySetInt32x4XYZW ||
         op_ == kInt8ArraySetInt32x4X ||
         op_ == kInt8ArraySetInt32x4XY ||
-        op_ == kInt8ArraySetInt32x4XYZ) {
+        op_ == kInt8ArraySetInt32x4XYZ ||
+        op_ == kUint8ArraySetInt32x4XYZW ||
+        op_ == kUint8ArraySetInt32x4X ||
+        op_ == kUint8ArraySetInt32x4XY ||
+        op_ == kUint8ArraySetInt32x4XYZ) {
       return Representation::Int32x4();
     }
     return RequiredValueRepresentation(elements_kind(), store_mode());
@@ -7322,12 +7342,18 @@ class HStoreKeyed FINAL
         op_ == kInt8ArraySetFloat32x4XYZW ||
         op_ == kInt8ArraySetFloat32x4X ||
         op_ == kInt8ArraySetFloat32x4XY ||
-        op_ == kInt8ArraySetFloat32x4XYZ) {
+        op_ == kInt8ArraySetFloat32x4XYZ ||
+        op_ == kUint8ArraySetFloat32x4XYZW ||
+        op_ == kUint8ArraySetFloat32x4X ||
+        op_ == kUint8ArraySetFloat32x4XY ||
+        op_ == kUint8ArraySetFloat32x4XYZ) {
       return Representation::Float32x4();
     } else if (op_ == kFloat64ArraySetFloat64x2XY ||
         op_ == kFloat64ArraySetFloat64x2X ||
         op_ == kInt8ArraySetFloat64x2XY ||
-        op_ == kInt8ArraySetFloat64x2X) {
+        op_ == kInt8ArraySetFloat64x2X ||
+        op_ == kUint8ArraySetFloat64x2XY ||
+        op_ == kUint8ArraySetFloat64x2X) {
       return Representation::Float64x2();
     } else if (op_ == kInt32ArraySetInt32x4XYZW ||
         op_ == kInt32ArraySetInt32x4X ||
@@ -7336,7 +7362,11 @@ class HStoreKeyed FINAL
         op_ == kInt8ArraySetInt32x4XYZW ||
         op_ == kInt8ArraySetInt32x4X ||
         op_ == kInt8ArraySetInt32x4XY ||
-        op_ == kInt8ArraySetInt32x4XYZ) {
+        op_ == kInt8ArraySetInt32x4XYZ ||
+        op_ == kUint8ArraySetInt32x4XYZW ||
+        op_ == kUint8ArraySetInt32x4X ||
+        op_ == kUint8ArraySetInt32x4XY ||
+        op_ == kUint8ArraySetInt32x4XYZ) {
       return Representation::Int32x4();
     }
     Representation r =
