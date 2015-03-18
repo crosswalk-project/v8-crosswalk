03aa302c7a12ab9dc5ef080d792378c1d3fd2654
chuan9 <chuan.liu@intel.com>
2015-03-18 12:10:31 +0800

    [SIMD.js] Fix no exception thrown when get invalid index of Float32x4Array. Add test case for exception thrown when get invalid index of Float32x4Array.
    BUG=XWALK-2440

--- src/simd128.js
+++ src/simd128.js
@@ -854,6 +854,9 @@ SetupSIMD128TypedArrays();
 
 macro DECLARE_TYPED_ARRAY_FUNCTION(NAME)
 function NAMEArrayGet(i) {
+  if((i < 0) || (i + 1 > this.length)) {
+    throw MakeRangeError('the index of value:' + i + ' ' + 'is invalid');
+  }
   return this[i];
 }
 
